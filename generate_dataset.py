540f76a74b7f9bc806a803662a872f2f8f93a490
Konstantinos Sardelis <csps0097@gmail.com>
2024-01-13 15:23:15 +0200

    Add files via upload

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,11 +4,13 @@ import argparse
 import subprocess
 import cv2
 import numpy as np
+H,W = 360,640
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Video Stabilization using DMBVS-UNet')
     parser.add_argument('--method', type=str, help='generation method')
     parser.add_argument('--stable_path', type=str, help='Directory of stable videos')
+    parser.add_argument('--cropped_stable_path', type=str, help='Directory of cropped stable videos')
     parser.add_argument('--unstable_path', type=str, help='Directory to generate unstable  videos')
     parser.add_argument('--transforms_path', type=str, help='Directory to generate transforms')
     return parser.parse_args()
@@ -20,27 +22,24 @@ def fixBorder(frame):
     frame = cv2.warpAffine(frame, T, (s[1], s[0]))
     return frame
 
-def crop_video(path):
-    cap = cv2.VideoCapture(path)
-    frames = []
+def crop_video(in_path,cropped_path):
+    cap = cv2.VideoCapture(in_path)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(cropped_path, fourcc, 30.0, (W,H))
     while True:
         ret,img = cap.read()
         if not ret: break
-        frames.append(img)
-    cap.release()
-    frames = np.array(frames, dtype = np.uint8)
-    num_frames,height,width,_ = frames.shape
-    cropped_frames = np.array([fixBorder(x) for x in frames],dtype = np.uint8)
-    output_path = path
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Adjust codec if needed
-    out = cv2.VideoWriter(output_path, fourcc, 30.0, (width, height))
-    for frame in cropped_frames: out.write(frame)
+        img = cv2.resize(img,(W,H))
+        img = fixBorder(img)
+        out.write(img)
     out.release()
 
 
-def generate(method,s_dir, out_dir, transform_dir):
+def generate(method,s_dir, out_dir, cropped_dir, transform_dir):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
+    if not os.path.exists(cropped_dir):
+        os.makedirs(cropped_dir)
     if not os.path.exists(transform_dir):
         os.makedirs(transform_dir)
 
@@ -59,10 +58,11 @@ def generate(method,s_dir, out_dir, transform_dir):
     for video in tqdm(videos):
         in_path = os.path.join(s_dir,video)
         out_path = os.path.join(out_dir,video)
+        cropped_path = os.path.join(cropped_dir,video)
         transform_path = os.path.join(transform_dir,video[:-4]+'.npy')
         subprocess.run(['python', script, '--in_path', in_path, '--out_path', out_path,'--transforms_path',transform_path])
-        crop_video(in_path) # crop original stable video to avoid perspective mismatch
+        crop_video(in_path,cropped_path) # crop original stable video to avoid perspective mismatch
 
 if __name__ == '__main__':
     args = parse_args()
-    generate(args.method, args.stable_path, args.unstable_path, args.transforms_path)
+    generate(args.method, args.stable_path, args.unstable_path, args.cropped_stable_path, args.transforms_path)
